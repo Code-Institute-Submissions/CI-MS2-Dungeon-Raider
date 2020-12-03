0a1b12bc6852686f88a7b880e55318aeecf6f520
scottsimpson91 <scottsimpson7@googlemail.com>
2020-12-03 11:39:59 +0000

    Feat: Add switch case and update Arthur img name/path

--- assets/js/gamemanager.js
+++ assets/js/gamemanager.js
@@ -1,10 +1,14 @@
 let GameManager = {
-    setGameStart: function() {
-        this.resetPlayer();
+    setGameStart: function(classType) {
+        this.resetPlayer(classType);
         this.setPreFight();
     },
-    resetPlayer: function() {
-
+    resetPlayer: function(classType) {
+        switch (classType) {
+            case "Arthur":
+                player = new Player(classType, 500, 100, 200, 150, 150);
+                break;
+        }
     },
     setPreFight: function() {
 
