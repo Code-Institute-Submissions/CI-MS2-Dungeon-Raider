64805127c3cc121d8698575e7f0da5575068fb71
scottsimpson91 <scottsimpson7@googlemail.com>
2020-12-03 10:19:28 +0000

    Feat: Add enemy function and declare enemy and player in global scope

---
+++ assets/js/enemy.js
@@ -0,0 +1,10 @@
+let enemy;
+
+function Enemy(enemyType, health, stamina, strength, agility, speed) {
+    this.enemyType = enemyType;
+    this.health = health;
+    this.stamina = stamina;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
+}
