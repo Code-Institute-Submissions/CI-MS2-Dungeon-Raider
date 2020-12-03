9fda24be8bf4f5bd7546e6864aff42ea14f29458
scottsimpson91 <scottsimpson7@googlemail.com>
2020-12-03 14:14:39 +0000

    Feat: Add player attack function, if else statement and offsetDamage calculation

--- assets/js/player.js
+++ assets/js/player.js
@@ -15,4 +15,14 @@ let PlayerMoves = {
         let getPlayerSpeed = player.speed;
         let getEnemySpeed = enemy.speed;
     }
+    // Player attacks
+    let playerAttack = function() {
+        let calcBaseDamage;
+        if (player.stamina > 0){
+            calcBaseDamage = player.strength * player.stamina / 1000;
+        } else {
+            calcBaseDamage = player.strength * player.agility / 1000;
+        }
+        let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+    }
 }
