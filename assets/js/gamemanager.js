8eded9038d9f6378cc95b91726c1a70752c5b32b
scottsimpson91 <scottsimpson7@googlemail.com>
2020-12-03 11:58:48 +0000

    Feat: Add getInterface and HTML text to be inserted

--- assets/js/gamemanager.js
+++ assets/js/gamemanager.js
@@ -9,6 +9,8 @@ let GameManager = {
                 player = new Player(classType, 500, 100, 200, 150, 150);
                 break;
         }
+        let getInterface = document.querySelector(".interface");
+        getInterface.innerHTML = '<img src="assets/images/' + classType.toLowerCase() + '.png" class="img-avatar"><div><h3>' + classType + '</h3><p>Health: ' + player.health + '</p><p>Stamina: ' + player.stamina + '</p><p>Strength: ' + player.strength + '</p><p>Agility: ' + player.agility + '</p><p>Speed: ' + player.speed + '</p></div>';
     },
     setPreFight: function() {
 
