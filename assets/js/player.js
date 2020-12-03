9f8139e0a52fbea17fffc3b70acd53f4f9af38ae
scottsimpson91 <scottsimpson7@googlemail.com>
2020-12-03 10:15:25 +0000

    Feat: Add Player function and GameManager

---
+++ assets/js/player.js
@@ -0,0 +1,8 @@
+function Player(classType, health, stamina, strength, agility, speed) {
+    this.classType = classType;
+    this.health = health;
+    this.stamina = stamina;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
+}
