df6b88ffc1fee68b9c2d4beb7295d0033fba75e7
scottsimpson91 <scottsimpson7@googlemail.com>
2020-12-07 11:21:17 +0000

    Feat: Add else if statement for if enemy is faster than player

--- assets/js/player.js
+++ assets/js/player.js
@@ -74,5 +74,30 @@ let PlayerMoves = {
                 }
             }
         }
+        else if (getPlayerSpeed >= getEnemySpeed) {
+            let enemyAttackValues = enemyAttack();
+            let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+            player.health = player.health - totalDamage;
+            alert("Enemy hit " + enemyAttackValues[0] + " damage " + enemyAttackValues[1] + " times.");
+            if (player.health <= 0) {
+                alert("You lose! Refresh browser to play again.");
+                getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+                getPlayerHealth.innerHTML = 'Health: 0';
+            } else {
+                getPlayerHealth.innerHTML = 'Health: ' + player.health;
+                // Player attacks
+                let playerAttackValues = playerAttack();
+                let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+                enemy.health = enemy.health - totalDamage;
+                alert("You hit " + playerAttackValues[0] + " damage " + playerAttackValues[1] + " times.");
+                if (enemy.health <= 0) {
+                    alert("You win! Refresh browser to play again");
+                    getEnemyHealth.innerHTML = 'Health: 0';
+                    getPlayerHealth.innerHTML = 'Health: ' + player.health;
+                } else {
+                    getEnemyHealth.innerHTML = 'Health: ' + enemy.health;
+                }
+            }
+        }        
     }
 }
