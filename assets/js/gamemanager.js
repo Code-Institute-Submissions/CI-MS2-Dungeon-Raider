fa7de73dba7be1aba72170a199cab401480c4996
scottsimpson91 <scottsimpson7@googlemail.com>
2020-12-08 08:11:03 +0000

    Feat: Adjust some of the enemy stats to balance out game better

--- assets/js/gamemanager.js
+++ assets/js/gamemanager.js
@@ -28,10 +28,10 @@ let GameManager = {
         // Create enemy
         let enemy00 = new Enemy("Goblin", 200, 100, 100, 225, 150);
         let enemy01 = new Enemy("Troll", 300, 100, 175, 175, 125);
-        let enemy02 = new Enemy("Golem", 500, 100, 300, 75, 75);
+        let enemy02 = new Enemy("Golem", 500, 100, 350, 75, 75);
         let enemy03 = new Enemy("Giant", 500, 100, 400, 50, 75);
         let enemy04 = new Enemy("Orc", 400, 100, 200, 125, 125);
-        let enemy05 = new Enemy("Dragon", 600, 100, 450, 200, 100);
+        let enemy05 = new Enemy("Dragon", 550, 100, 400, 200, 100);
         
         let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(6));
         switch (chooseRandomEnemy) {
